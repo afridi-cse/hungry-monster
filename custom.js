0f23bc57fdfc029d9255e0492873cb2ec2203a96
Shahid Afridi <shahidafridi.cse@gmail.com>
2021-02-09 17:44:48 +0600

    update

--- custom.js
+++ custom.js
@@ -1,12 +1,25 @@
-const searchMeal = () => {
+const searchMeal = async() => {
     const searchText = document.getElementById('searchInput').value;
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayMeals(data.meals))
+    if (searchText === "") {
+        displayError(" Your searched food is not available in our cart.");
+    } else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            displayMeals(data.meals);
+            displayError("");
+        } catch (error) {
+            displayError(" Your searched food is not available in our cart.");
+        }
+
+    }
+
+
 }
 const displayMeals = (meals) => {
     const mealsContainer = document.getElementById("mealsContainer");
+    mealsContainer.innerHTML = '';
     meals.forEach(meal => {
         const mealDiv = document.createElement('div');
         mealDiv.className = 'mealDiv col';
@@ -22,12 +35,20 @@ const displayMeals = (meals) => {
 
     });
 }
-const displayIngredients = (id) => {
+const displayIngredients = async(id) => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => ingredientsInfo(data.meals[0]));
+    const res = await fetch(url);
+    const data = await res.json();
+    ingredientsInfo(data.meals[0]);
 }
+
+
+// const displayIngredients = (id) => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => ingredientsInfo(data.meals[0]));
+// }
 
 const ingredientsInfo = meals => {
     const ingredientsDiv = document.getElementById("displayIngredients");
@@ -53,4 +74,13 @@ const ingredientsInfo = meals => {
     `;
 
 
+}
+const displayError = (error) => {
+    console.log("error");
+    const errorText = document.getElementById('error-messege');
+
+    errorText.innerHTML = `
+    <h1>${error}</h1>
+
+   `
 }
